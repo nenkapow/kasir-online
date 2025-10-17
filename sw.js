433569f49929870c532cc44e41d8a237b17b64e0
nenkapow <156160101+nenkapow@users.noreply.github.com>
2025-10-17 16:50:11 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,19 @@
-const CACHE='kasir-cache-v1';
-const ASSETS=[
-  '/', 'index.html','report.html',
+const CACHE = 'kasir-cache-v5';
+const ASSETS = [
+  '/',
+  'index.html',
+  'report.html',
   'assets/css/style.css',
-  'assets/js/app.js','assets/js/report.js',
+  'assets/js/app.js',
+  'assets/js/report.js',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js'
 ];
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
-self.addEventListener('fetch', e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=> r || fetch(e.request))
-  );
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
